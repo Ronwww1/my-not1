638b71269c0dd5cea91cba9703a31f0a9edad79b
Ronwww1 <144040286+Ronwww1@users.noreply.github.com>
2024-11-22 11:52:54 +0500

    Update zod.ts

--- types/zod.ts
+++ types/zod.ts
@@ -3,8 +3,11 @@ import { z } from "zod";
 export const fileUploadFormSchema = z.object({
   name: z
     .string()
-    .min(1)
-    .max(50)
-    .regex(/^[a-zA-Z ]+$/, "Only letters and spaces are allowed"),
-  type: z.string().min(1).max(50),
+    .min(1, "Name must be at least 1 character long")
+    .max(50, "Name must be at most 50 characters long")
+    .regex(/^[a-zA-Z ]+$/, "Name must contain only letters and spaces"),
+  type: z
+    .string()
+    .min(1, "Type must be at least 1 character long")
+    .max(50, "Type must be at most 50 characters long"),
 });
