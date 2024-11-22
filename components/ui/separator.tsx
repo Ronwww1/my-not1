568ed75ddec5806e139629b23b9baa27fc254c0b
Ronwww1 <144040286+Ronwww1@users.noreply.github.com>
2024-11-22 11:13:13 +0500

    update separator.tsx

--- components/ui/separator.tsx
+++ components/ui/separator.tsx
@@ -1,7 +1,17 @@
-"use client"
+"🐾🐾use client"
 
 import * as React from "react"
 import * as SeparatorPrimitive from "@radix-ui/react-separator"
+
+
+
+import * as React from "react"
+import * as SeparatorPrimitive from "@radix-ui/react-separator"
+
+
+import * as React from "react"
+import * as SeparatorPrimitive from "@radix-ui/react-separator"
+
 
 import { cn } from "@/lib/utils"
 
